4ee8c4552a97fb18f4caf5732e4d9d4da7b483cb
RuoningJin <ronjin2013@hotmail.com>
2023-01-04 17:03:18 -0500

    homepage setup

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Button.scss';
+
+export const Button = () => {
+  return (
+    <button type="button" className={'confirm'}>
+      <span class="red">S</span>
+      <span class="blue">t</span>
+      <span class="yellow">a</span>
+      <span class="green">r</span>
+      <span class="red">t</span>
+    </button>
+  );
+};
---
+++ src/components/Continue.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Button.scss';
+
+export const Continue = () => {
+  return (
+    <button type="button" className={'confirm'}>
+      <span class="red">C</span>
+      <span class="blue">o</span>
+      <span class="yellow">n</span>
+      <span class="green">t</span>
+      <span class="green">i</span>
+      <span class="yellow">n</span>
+      <span class="blue">u</span>
+      <span class="red">e</span>
+    </button>
+  );
+};
