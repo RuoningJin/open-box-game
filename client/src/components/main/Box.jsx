53f018ba1c8d497bfadf5d411cd6ca10dd9aacd5
RuoningJin <ronjin2013@hotmail.com>
2023-02-01 20:10:53 -0500

    add change of state to close->openbox

--- client/src/components/main/Box.jsx
+++ client/src/components/main/Box.jsx
@@ -1,11 +1,16 @@
+import { useState } from 'react';
+
 import './Box.scss';
 import closeBox from '../../img/close_box_red_square.png';
+import openBox from '../../img/open_box_red_square.png';
 
 export default function Box() {
+  const [box, setBox] = useState(closeBox);
+  const clickHandler = () => {setBox(openBox)}
 
   return (
     <section>
-      <img className='box' src={closeBox} alt='open box'/>
+      <img className='box' src={box} alt='open box' onClick={clickHandler}/>
     </section>
   )
 }
--- client/src/components/main/index.js
+++ client/src/components/main/index.js
@@ -1,10 +1,11 @@
+import './index.scss';
 import Box from "./Box";
 
 export default function Trials() {
 
   return (
-    <>
+    <main className='main-layout'>
       <Box />
-    </>
+    </main>
   )
 }
