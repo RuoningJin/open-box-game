2e87b5e147c32efdc363bea09baf26751340e1c5
RuoningJin <ronjin2013@hotmail.com>
2023-02-02 21:04:58 -0500

    implement fullscreen for main page

--- client/src/components/main/index.js
+++ client/src/components/main/index.js
@@ -1,11 +1,25 @@
+import { useEffect } from 'react';
 import './index.scss';
 import Box from "./Box";
 
 export default function Trials() {
 
+  const elem = document.getElementById("fullScreen");
+
+  useEffect(() => {
+
+  }, [])
+  const openFullScreen = () => {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    }
+  }
+
   return (
-    <main className='main-layout'>
-      <Box />
+    <main id='fullScreen' className='main-layout' onClick={openFullScreen}>
+      <div className='box-container'>
+        <Box />
+      </div>
     </main>
   )
 }
