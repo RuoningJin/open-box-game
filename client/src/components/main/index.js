094f86b04a7387cccde32533de9b87619c723253
RuoningJin <ronjin2013@hotmail.com>
2023-02-03 16:09:08 -0500

    implement fullscreen functionality at game play

--- client/src/components/main/index.js
+++ client/src/components/main/index.js
@@ -1,27 +1,36 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './index.scss';
+import '../Button.scss';
+
 import Box from "./Box";
 
 export default function Trials() {
   const ref = useRef(null);
+  const [fullScreen, setFullScreen] = useState(false);
 
-  // componentDidMount() {
-  //   if (ref.current.requestFullscreen) {
-  //     ref.current.requestFullscreen();
-  //   }
-  // }
-
-  useEffect(() => {
-    if (ref.current.requestFullscreen) {
-      ref.current.requestFullscreen();
-    }
-  }, [])
+  const openFullScreen = () => {
+    ref.current.requestFullscreen();
+    setFullScreen(true);
+  }
 
   return (
     <main ref={ref} className='main-layout'>
-      <div className='box-container'>
+      {fullScreen === false && 
+      <button onClick={openFullScreen} className='play-button'>      
+        <span className="red">L</span>
+        <span className="blue">e</span>
+        <span className="yellow">t'</span>
+        <span className="green">s </span>
+        <span className="red">P</span>
+        <span className="blue">l</span>
+        <span className="yellow">a</span>
+        <span className="green">y!</span>
+      </button>}
+      {fullScreen === true &&
+        <div className='box-container'>
         <Box />
       </div>
+      }
     </main>
   )
 }
