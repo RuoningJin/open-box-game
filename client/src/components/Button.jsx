6dad522154b83a903c78e4429795e20850c1b954
RuoningJin <ronjin2013@hotmail.com>
2023-04-18 16:35:37 -0400

    change background and confirm-button styles

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -4,11 +4,7 @@ import './Button.scss';
 export default function Button (props) {
   return (
     <button type="button" className={'confirm'} onClick={props.onClick}>
-      <span className="red">S</span>
-      <span className="blue">t</span>
-      <span className="yellow">a</span>
-      <span className="green">r</span>
-      <span className="red">t</span>
+      <span>Start</span>
     </button>
   );
 };
