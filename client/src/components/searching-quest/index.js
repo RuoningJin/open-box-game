92e57b5c6ef11753b89cba43e3a96827a8c983a7
RuoningJin <ronjin2013@hotmail.com>
2023-04-26 16:59:15 -0400

    create Quests component

--- client/src/components/searching-quest/index.js
+++ client/src/components/searching-quest/index.js
@@ -3,9 +3,9 @@ import Loading from './Loading';
 
 export default function Searching(props) {
   const [loading, setLoading] = useState(true);
-  // setTimeout(() => {
-  //   setLoading(false);
-  // }, 5000);
+  setTimeout(() => {
+    setLoading(false);
+  }, 7000);
 
   return (
     <>
