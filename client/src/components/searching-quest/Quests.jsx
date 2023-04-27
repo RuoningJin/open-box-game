10d647ee3e8149047aa2a84739b720d1b5a00916
RuoningJin <ronjin2013@hotmail.com>
2023-04-26 20:20:48 -0400

    add cloud background to quest component

---
+++ client/src/components/searching-quest/Quests.jsx
@@ -0,0 +1,12 @@
+import './Quests.scss';
+
+export default function Quests(props) {
+
+  return (
+    <>
+      <div className='quests'>
+        <img src='/city-map.png' alt='quests-map' className='quests-map'/>
+      </div>
+    </>
+  )
+}
--- client/src/components/searching-quest/index.js
+++ client/src/components/searching-quest/index.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react';
 import Loading from './Loading';
+import Quests from './Quests';
+
+import './index.scss';
 
 export default function Searching(props) {
   const [loading, setLoading] = useState(true);
@@ -9,8 +12,10 @@ export default function Searching(props) {
 
   return (
     <>
-      {loading &&  <Loading />}
+      <main className='background-cloud'>
+        {loading &&  <Loading />}
+        {!loading && <Quests />}
+      </main>
     </>
-   
   )
 }
