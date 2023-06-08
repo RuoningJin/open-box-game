c1aeda5931087110acb084ec4fb12994b9a8e9af
RuoningJin <ronjin2013@hotmail.com>
2023-06-07 23:06:47 -0400

    adding moving ocean and boat into the main background

---
+++ client/src/components/main/FishingBackground.jsx
@@ -0,0 +1,23 @@
+import './FishingBackground.scss';
+
+export default function FishingBackground(props) {
+
+  return (
+    <section className='fishing-background'>
+      <div className='boat'></div>
+      
+      <div className='fish fish1'></div>
+      <div className='fish fish2'></div>
+      <div className='fish fish3'></div>
+      <div className='fish fish4'></div>
+      <div className='fish fish5'></div>
+      <div className='fish fish6'></div>
+
+      <div className='wave-bottom'></div>
+      <div className='wave wave1'></div>
+      <div className='wave wave2'></div>
+      <div className='wave wave3'></div>
+      <div className='wave wave4'></div>
+    </section>
+  )
+}
