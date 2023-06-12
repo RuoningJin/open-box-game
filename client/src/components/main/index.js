6fe1b97911553019b2aa4f2460c9d71242744861
RuoningJin <ronjin2013@hotmail.com>
2023-06-12 13:38:40 -0400

    adding a choose bait section

--- client/src/components/main/index.js
+++ client/src/components/main/index.js
@@ -7,6 +7,15 @@ export default function Game(props) {
 
   return (
     <>
+      <div className='bait-select'>
+        <div className='bait-select--title'>
+          <span className="red">Cho</span>
+          <span className="blue">ose</span>
+          <span className="green"> Your</span>
+          <span className="yellow"> Bait</span>
+        </div>
+      </div>
+
       <OpponentScreen />
       <FishingBackground />
     </>
