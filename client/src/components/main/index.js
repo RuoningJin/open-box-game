86d49887ad96d5bd4d3db23e4fbc086e8ffc40e1
RuoningJin <ronjin2013@hotmail.com>
2023-06-14 20:12:02 -0400

    luminate bait selection

--- client/src/components/main/index.js
+++ client/src/components/main/index.js
@@ -1,12 +1,28 @@
+import { useState } from 'react';
 import './index.scss';
 
 import FishingBackground from "./FishingBackground";
 import OpponentScreen from './OpponentScreen';
 
 export default function Game(props) {
+  const [chooseBait, setChooseBait] = useState(true);
+
+  const [bait, setBait] = useState('');
+
+  // const left = classNames('gender', {
+  //   'girl-selected': gender === 'girl',
+  // });
+  // const middle = classNames('gender', {
+  //   'boy-selected': gender === 'boy',
+  // });
+
+  const chooseBaitHandler = () => {
+    setChooseBait(false);
+  }
 
   return (
     <>
+    {chooseBait &&
       <div className='bait-select'>
         <div className='bait-select--title'>
           <span className="red">Cho</span>
@@ -16,11 +32,21 @@ export default function Game(props) {
         </div>
 
         <div className='bait-select--baits'>
-          <img src="./baits/banana.png" alt="bait"/>
-          <img src="./baits/banana.png" alt="bait"/>
-          <img src="./baits/banana.png" alt="bait"/>
+          <img className='left' src="./baits/banana.png" alt="bait"/>
+          <img className='middle' src="./baits/banana.png" alt="bait"/>
+          <img className='right' src="./baits/banana.png" alt="bait"/>
+        </div>
+
+        <div className='bait-select--button' onClick={chooseBaitHandler}>
+          <div>
+            <span className="red">G</span>
+            <span className="blue">o</span>
+            <span className="yellow">!</span>
+          </div>
         </div>
       </div>
+    }
+      
 
       <OpponentScreen />
       <FishingBackground />
