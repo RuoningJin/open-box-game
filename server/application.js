59d84c4f98030ce452515ef100042c6ffe4acfa5
RuoningJin <ronjin2013@hotmail.com>
2023-07-21 22:08:37 -0400

    setting up database with schema and seeds

--- server/application.js
+++ server/application.js
@@ -1,70 +1,8 @@
 const express = require('express');
 const app = express();
+const fs = require("fs");
 
 const cors = require('cors');
-
-const trials = {
-  1: {
-    shape: 'square',
-    color: 'red',
-    valence: 'positive',
-    rewards: 'positive'
-  },
-  2: {
-    shape: 'triangle',
-    color: 'blue',
-    valence: 'neutral',
-    rewards: 'negative'
-  },
-  3: {
-    shape: 'circle',
-    color: 'green',
-    valence: 'negative',
-    rewards: ''
-  },
-  4: {
-    shape: 'circle',
-    color: 'blue',
-    valence: 'neutral',
-    rewards: 'positive'
-  },
-  5: {
-    shape: 'circle',
-    color: 'red',
-    valence: 'positive',
-    rewards: ''
-  },
-  6: {
-    shape: 'triangle',
-    color: 'green',
-    valence: 'neutral',
-    rewards: 'negative'
-  },
-  7: {
-    shape: 'diamond',
-    color: 'blue',
-    valence: 'negative',
-    rewards: ''
-  },
-  8: {
-    shape: 'square',
-    color: 'green',
-    valence: 'neutral',
-    rewards: ''
-  },
-  9: {
-    shape: 'diamond',
-    color: 'blue',
-    valence: 'negative',
-    rewards: 'negative'
-  },
-  10: {
-    shape: 'circle',
-    color: 'green',
-    valence: 'negative',
-    rewards: ''
-  },
-}
 
 app.use(cors({ origin: 'http://localhost:3000'}));
 
--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,11 @@
-const pg = require("pg");
+import { Client } from 'pg'
 
-const client = new pg.Client({
+const client = new Client({
   connectionString: process.env.DATABASE_URL || "",
   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
-client
+await client
   .connect()
   .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
 
