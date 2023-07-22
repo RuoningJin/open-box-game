71da063a687946e9c0c6b80a9894603a7b748b47
RuoningJin <ronjin2013@hotmail.com>
2023-07-22 16:27:12 -0400

    modifying the query to include properties of each categories

--- server/src/routes/trials.js
+++ server/src/routes/trials.js
@@ -2,7 +2,27 @@ const router = require("express").Router();
 
 module.exports = db => {
   router.get("/trials", (request, response) => {
-    db.query(`SELECT * FROM trials`).then(({ rows: trials }) => {
+    db.query(`
+      SELECT
+        t.id AS trial_id,
+        t.session,
+        t.bait_1_category,
+        c1.name AS bait_1_category_name,
+        c1.positive AS bait_1_positive,
+        c1.reward AS bait_1_reward,
+        t.bait_2_category,
+        c2.name AS bait_2_category_name,
+        c2.positive AS bait_2_positive,
+        c2.reward AS bait_2_reward,
+        t.bait_3_category,
+        c3.name AS bait_3_category_name,
+        c3.positive AS bait_3_positive,
+        c3.reward AS bait_3_reward
+        FROM trials t
+        JOIN categories c1 ON t.bait_1_category = c1.id
+        JOIN categories c2 ON t.bait_2_category = c2.id
+        JOIN categories c3 ON t.bait_3_category = c3.id;
+    `).then(({ rows: trials }) => {
       response.json(trials);
     });
   });
